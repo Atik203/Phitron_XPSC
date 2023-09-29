13b5c70c2d775748d5bc9b5df547447d2fa47c70
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-09-29 23:32:18 +0600

    first commit

---
+++ Week 1/Class 1 - Sliding Window/max_sum_subArray.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fori(i, s, n) for (i = s; i < n; i++)
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    int i;
+    fori(i, 0, n) cin >> a[i];
+    int k;
+    cin >> k;
+
+    int mx = INT_MIN;
+    int j;
+
+    for (i, 0, n - k + 1)
+    {
+        int s = 0;
+        for (j, 0, i + k)
+        {
+            s += a[j];
+        }
+        mx = max(mx, s);
+    }
+
+    cout << mx << endl;
+    return 0;
+}
