5be2cecb29dc52dd62af993219977f3cd77180a2
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-09-30 15:34:19 +0600

    At-coder problem

---
+++ Week 1/A_Discount.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a, b;
+    cin >> a >> b;
+    double ans = (((a - b) * 100) / a);
+    cout << setprecision(15) << ans << endl;
+    return 0;
+}
