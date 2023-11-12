bb7fe26bd531dd8c5f807e9f7406b3cdb7a3d8ee
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-11-13 00:16:37 +0600

    codechef problem

---
+++ Week 7/Practice Problem/AP_Free_Sequences.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool ap(vector<int> seq, int n)
+{
+    for (int i = 0; i < n - 2; i++)
+    {
+        for (int j = i + 1; j < n - 1; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+            {
+                if (seq[j] - seq[i] == seq[k] - seq[j])
+                    return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while (T--)
+    {
+
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+
+        if (ap(arr, n))
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
---
+++ Week 7/Practice Problem/Printing_Binary_Array.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a;
+            if (a == 0)
+                cout << 1 << " ";
+            else
+                cout << 0 << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
