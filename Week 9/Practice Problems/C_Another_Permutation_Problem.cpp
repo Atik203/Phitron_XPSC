53777112c834e07a4b936858996690eb4febe921
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-11-27 00:48:38 +0600

    codeforces

---
+++ Week 9/Practice Problems/C_Another_Permutation_Problem.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define endl '\n'
+#define f(i, s, n) for (long long int i = s; i < n; i++)
+#define cf(i, s, n) for (long long int i = s; i <= n; i++)
+#define rf(i, n, s) for (long long int i = e - 1; i >= n; i--)
+#define pb push_back
+#define eb emplace_back
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+
+    {
+        int n;
+        cin >> n;
+
+        ll arr[n];
+        for (ll i = 0; i < n; i++)
+            arr[i] = 1;
+        ll ans = 0;
+        for (ll i = 0; i <= n; i++)
+        {
+            ll a = 0, b = 0;
+            for (ll j = 0; j < i; j++)
+            {
+                a += (j + 1) * (j + 1);
+                b = max(b, (j + 1) * (j + 1));
+            }
+            for (ll j = i; j < n; j++)
+            {
+                a += (j + 1) * (n - j + i);
+                b = max(b, (j + 1) * (n - j + i));
+            }
+            ans = max(a - b, ans);
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
