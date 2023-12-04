c414402ffccc009d28c2223b48df4b1f725c327a
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-12-04 23:26:46 +0600

    contest

---
+++ Week 10/contest/A_Array_Stabilization.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define endl '\n'
+#define f(i, s, n) for (long long int i = s; i < n; i++)
+#define cf(i, s, n) for (long long int i = s; i <= n; i++)
+#define rf(i, n, s) for (long long int i = e - 1; i >= n; i--)
+#define pb push_back
+#define eb emplace_back
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    vi a(n);
+    f(i, 0, n)
+    {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+    int ans = min(a[n - 2] - a[0], a[n - 1] - a[1]);
+    cout << ans << endl;
+
+    return 0;
+}
