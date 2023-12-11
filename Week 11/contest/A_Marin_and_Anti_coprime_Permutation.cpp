305b0af0bcfade56f546d8bd20aef21e0a5720b7
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-12-11 23:52:19 +0600

    contest

---
+++ Week 11/contest/A_Marin_and_Anti_coprime_Permutation.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define endl '\n'
+#define f(i, s, n) for (long long int i = s; i < n; i++)
+#define cf(i, s, n) for (long long int i = s; i <= n; i++)
+#define rf(i, s, n) for (long long int i = n - 1; i >= s; i--)
+#define pb push_back
+#define eb emplace_back
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define all(a) sort(a.begin(), a.end())
+#define All(a, n) sort(a, a + n);
+#define rall(a) sort(a.rbegin(), a.rend())
+
+ll fact(ll n)
+{
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    return (n * fact(n - 1)) % 998244353;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    if (n % 2)
+    {
+        cout << 0 << endl;
+
+        return;
+    }
+    ll x = n / 2;
+    ll result = (fact(x) * fact(x)) % 998244353;
+    cout << result << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
