37e569ff6e2a45cecf28aeeec0f2bec08813150f
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2024-01-05 22:52:16 +0600

    codeforces

---
+++ Problems From Sheet/B_Closest_to_the_Left.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    for (auto &i : a)
+        cin >> i;
+
+    for (ll i = 0; i < k; ++i)
+    {
+        ll x;
+        cin >> x;
+        auto it = upper_bound(a.begin(), a.end(), x);
+        cout << it - a.begin() << '\n';
+    }
+
+    return 0;
+}
