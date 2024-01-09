91e1dfe955cebea3d9a807c598edfc10c58d8ad7
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2024-01-09 21:42:14 +0600

    codeforces

---
+++ Problems From Sheet/G_1_Subsequence_Addition_Easy_Version.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define endl '\n'
+#define f(i, s, n) for (long long int i = s; i < n; i++)
+#define cf(i, s, n) for (long long int i = s; i <= n; i++)
+#define rf(i, s, n) for (long long int i = n - 1; i >= s; i--)
+#define pb push_back
+#define eb emplace_back
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define all(a) sort(a.begin(), a.end())
+#define All(a, n) sort(a, a + n);
+#define rall(a) sort(a.rbegin(), a.rend())
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+    }
+    (all(a));
+    if (a[0] != 1)
+    {
+        cout << "NO\n";
+        return;
+    }
+    vector<int> dp(5005, 0);
+    dp[1] = 1;
+    for (int i = 1; i < n; ++i)
+    {
+        if (!dp[a[i]])
+        {
+            cout << "NO\n";
+            return;
+        }
+        for (int j = 5000; j >= a[i]; --j)
+        {
+            dp[j] |= dp[j - a[i]];
+        }
+    }
+    cout << "YES\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
