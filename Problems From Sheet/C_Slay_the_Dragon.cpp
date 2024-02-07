e7811c7b43a06ba2c10acb7c348a0f0ba9fbbde9
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2024-02-07 22:06:52 +0600

    codeforces

---
+++ Problems From Sheet/C_Slay_the_Dragon.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define endl '\n'
+#define f(i, s, n) for (long long int i = s; i < n; i++)
+#define cf(i, s, n) for (long long int i = s; i <= n; i++)
+#define rf(i, s, n) for (long long int i = n - 1; i >= s; i--)
+#define pb push_back
+#define eb emplace_back
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define all(a) sort(a.begin(), a.end())
+#define All(a, n) sort(a, a + n);
+#define rall(a) sort(a.rbegin(), a.rend())
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for (auto &x : a)
+        cin >> x;
+    sort(a.begin(), a.end());
+    ll sum = accumulate(a.begin(), a.end(), 0LL);
+    int m;
+    cin >> m;
+    while (m--)
+    {
+        ll x, y;
+        cin >> x >> y;
+        int i = lower_bound(a.begin(), a.end(), x) - a.begin();
+        ll ans = 2e18;
+        if (i > 0)
+            ans = min(ans, (x - a[i - 1]) + max(0LL, y - sum + a[i - 1]));
+        if (i < n)
+            ans = min(ans, max(0LL, y - sum + a[i]));
+        cout << ans << '\n';
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
