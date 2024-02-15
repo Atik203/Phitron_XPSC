0eb79de6ef756ca184c9803a4c478cd4448469da
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2024-02-15 23:26:42 +0600

    codeforces

---
+++ Problems From Sheet/D_Divisible_Pairs.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define endl '\n'
+#define f(i, s, n) for (long long int i = s; i < n; i++)
+#define cf(i, s, n) for (long long int i = s; i <= n; i++)
+#define rf(i, s, n) for (long long int i = n - 1; i >= s; i--)
+#define pb push_back
+#define eb emplace_back
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define all(a) sort(a.begin(), a.end())
+#define All(a, n) sort(a, a + n);
+#define rall(a) sort(a.rbegin(), a.rend())
+
+void solve()
+{
+    ll n, x, y;
+    cin >> n >> x >> y;
+    vll v(n);
+    f(i, 0, n)
+    {
+        cin >> v[i];
+    }
+    map<ll, map<ll, ll>> mp;
+    ll ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ans += mp[(x - v[i] % x) % x][v[i] % y];
+        mp[v[i] % x][v[i] % y]++;
+    }
+    cout << ans << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ Problems From Sheet/E_Klever_Permutation.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define endl '\n'
+#define f(i, s, n) for (long long int i = s; i < n; i++)
+#define cf(i, s, n) for (long long int i = s; i <= n; i++)
+#define rf(i, s, n) for (long long int i = n - 1; i >= s; i--)
+#define pb push_back
+#define eb emplace_back
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define all(a) sort(a.begin(), a.end())
+#define All(a, n) sort(a, a + n);
+#define rall(a) sort(a.rbegin(), a.rend())
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    int l = 1, h = n;
+    vector<int> v(n, 0);
+    for (int i = 0; i < k; ++i)
+    {
+        for (int j = i; j < n; j += k)
+        {
+            if (i & 1)
+                v[j] = h--;
+            else
+                v[j] = l++;
+        }
+    }
+    for (auto x : v)
+        cout << x << " ";
+    cout << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
