6f21f6fc958ffdfac5be0c7cce139ef0158395c3
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2024-02-22 23:31:49 +0600

    codeforces

--- Problems From Sheet/C_Vlad_and_a_Sum_of_Sum_of_Digits.cpp
+++ Problems From Sheet/C_Vlad_and_a_Sum_of_Sum_of_Digits.cpp
@@ -1,66 +1,40 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-#define ll long long
-#define pii pair<int, int>
-#define pll pair<long long, long long>
-#define vi vector<int>
-#define vll vector<long long>
-#define mii map<int, int>
-#define si set<int>
-#define sc set<char>
-#define endl '\n'
-#define f(i, s, n) for (long long int i = s; i < n; i++)
-#define cf(i, s, n) for (long long int i = s; i <= n; i++)
-#define rf(i, s, n) for (long long int i = n - 1; i >= s; i--)
-#define pb push_back
-#define eb emplace_back
-#define MOD 1000000007
-#define PI 3.1415926535897932384626433832795
-#define all(a) sort(a.begin(), a.end())
-#define All(a, n) sort(a, a + n);
-#define rall(a) sort(a.rbegin(), a.rend())
-ll sumOfDigits(ll n)
+
+const int MAX = 200'007;
+const int MOD = 1'000'000'007;
+
+int res[MAX];
+
+int S(int x)
 {
-    ll sum = 0;
-    while (n > 0)
+    int res = 0;
+    while (x)
     {
-        sum += n % 10;
-        n /= 10;
+        res += (x % 10);
+        x /= 10;
     }
-    return sum;
+    return res;
 }
-ll sumOfDigitsFrom1ToN(ll n)
-{
-    if (n < 10)
-    {
-        return n * (n + 1) / 2;
-    }
-    ll dp[n + 1];
-    dp[0] = 0;
-    for (ll i = 1; i < 10; i++)
-    {
-        dp[i] = dp[i - 1] + i;
-    }
-    for (ll i = 10; i <= n; i++)
-    {
-        dp[i] = dp[i - 1] + sumOfDigits(i);
-    }
-    return dp[n];
-}
+
 void solve()
 {
-    ll n;
-    cin >> n;
-    cout << sumOfDigitsFrom1ToN(n) << endl;
+    int x;
+    cin >> x;
+    cout << res[x] << '\n';
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t = 1;
-    cin >> t;
-    while (t--)
+    res[0] = 0;
+    for (int i = 1; i < MAX; i++)
+    {
+        res[i] = res[i - 1] + S(i);
+    }
+    int tt;
+    cin >> tt;
+    for (int i = 1; i <= tt; i++)
     {
         solve();
     }
